b53a5ec2700f298f5fdaa3b6c3bcecce23151151
Muhammad Zeeshan <mzeeshanid@yahoo.com>
2016-05-17 17:53:32 +0500

    - initial commit

--- NLP_Assignment3/ViewController.swift
+++ NLP_Assignment3/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import CHCSVParser
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let fileURL = NSBundle.mainBundle().URLForResource("labeledTrainData", withExtension: "tsv") else {
+            fatalError("Unable to locate csv file")
+        }
+        
+        let delimiter = "\t".utf16.first!
+        let options: CHCSVParserOptions = [.SanitizesFields, .TrimsWhitespace, .UsesFirstLineAsKeys, .RecognizesBackslashesAsEscapes]
+        do {
+            let parsedArray = try NSArray.init(contentsOfDelimitedURL: fileURL, options: options, delimiter: delimiter, error: ())
+            debugPrint("first diction: \(parsedArray.firstObject!, parsedArray.count)")
+        } catch let error as NSError {
+            debugPrint("error while parsing csv file: " + error.localizedDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,18 @@
+platform :ios, '8.0'
+
+target 'NLP_Assignment3' do
+  use_frameworks!
+
+  pod 'CHCSVParser', '~> 2.1.0'
+
+  target 'NLP_Assignment3Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'NLP_Assignment3UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
